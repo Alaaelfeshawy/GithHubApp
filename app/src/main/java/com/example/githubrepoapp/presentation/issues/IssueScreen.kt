707b0ed4845cf891b19ac7fba4386e9d8a584773
alaaelfeshawy <alaaelfeshawy@gmail.com>
2023-12-29 23:41:19 +0200

    implement issue list ui

--- app/src/main/java/com/example/githubrepoapp/presentation/issues/IssueScreen.kt
+++ app/src/main/java/com/example/githubrepoapp/presentation/issues/IssueScreen.kt
@@ -1,34 +1,43 @@
 package com.example.githubrepoapp.presentation.issues
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.githubrepoapp.presentation.appcomponents.AppScaffold
 import com.example.githubrepoapp.presentation.appcomponents.BaseErrorUI
+import com.example.githubrepoapp.presentation.appcomponents.EmptyScreen
+import com.example.githubrepoapp.presentation.issues.component.CreateIssueList
 
 @Composable
 fun IssueScreen(navController: NavController , owner : String? , repo : String?) {
 
     AppScaffold(title = "Repo Issues") {
-        val viewModel = hiltViewModel<IssuesViewModel>()
-        repo?.let {owner?.let { owner -> viewModel.getIssues(owner, it) } }
-        val state by viewModel.state.collectAsState()
-        if (state.isLoading == true){
-            CircularProgressIndicator()
-        }
-        if (state.data != null){
-            Column {
-                Text(text = state.data?.get(0)?.state ?: "")
-                Text(text = state.data?.get(0)?.title ?: "")
-                Text(text = state.data?.get(0)?.author ?: "")
-                Text(text = state.data?.get(0)?.createdAt ?: "")
+        innerPadding->
+        Column (modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),){
+            val viewModel = hiltViewModel<IssuesViewModel>()
+            repo?.let {owner?.let { owner -> viewModel.getIssues(owner, it) } }
+            val state by viewModel.state.collectAsState()
+            if (state.isLoading == true){
+                CircularProgressIndicator()
             }
+            if (state.data != null){
+                if (!state.data.isNullOrEmpty()){
+                    CreateIssueList(state.data!!)
+                }else{
+                    EmptyScreen()
+                }
+            }
+            state.errorView?.let { BaseErrorUI(it) }
         }
-        state.errorView?.let { BaseErrorUI(it) }
+
     }
 }
---
+++ app/src/main/java/com/example/githubrepoapp/presentation/issues/component/IssueComponent.kt
@@ -0,0 +1,108 @@
+package com.example.githubrepoapp.presentation.issues.component
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.githubrepoapp.R
+import com.example.githubrepoapp.data.network.models.IssueModel
+import com.example.githubrepoapp.presentation.extractDate
+
+@Composable
+fun CreateIssueList(list : List<IssueModel>) {
+    LazyColumn{
+        items(list){
+            IssueItem(it)
+        }
+    }
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun IssueItem(item : IssueModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .wrapContentHeight(),) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            RowItem(
+                title = stringResource(R.string.title),
+                desc = item.title ?: "" ,
+                modifier = Modifier.weight(1f)
+            )
+
+            RowItem(modifier = Modifier.weight(1f),
+                title = stringResource(R.string.author) ,
+                desc = item.author ?: "")
+
+            RowItem(modifier = Modifier.weight(1f),
+                title = stringResource(R.string.state),
+                desc = item.state ?: "")
+
+            RowItem(
+                modifier = Modifier.weight(1f) ,
+                title  = stringResource(R.string.created_at),
+                desc = extractDate(item.createdAt) ?: "")
+
+        }
+
+        Divider(
+            color = Color.LightGray,
+            modifier = Modifier.padding(2.dp)
+        )
+    }
+}
+
+@Composable
+fun Item(item:String) {
+    Text(
+        text = item,
+        textAlign = TextAlign.Start,
+        maxLines = 5,
+        style = TextStyle(
+            fontSize = 14.sp
+        ),
+        color = Color.Gray
+    )
+}
+
+@Composable
+fun RowItem(title : String , desc : String , modifier: Modifier) {
+    Column(modifier = modifier.padding(5.dp)) {
+        Header(text = title)
+        Item(desc)
+    }
+}
+
+@Composable
+private fun Header(text:String) {
+    Text(
+        modifier = Modifier
+            .padding(6.dp)
+            .fillMaxWidth(),
+        text = text, fontWeight = FontWeight.Bold,
+        style = TextStyle(fontSize = 16.sp),
+    )
+}
